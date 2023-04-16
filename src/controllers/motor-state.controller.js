36138ee351c45111ae11efa3894dda84984d7fc7
ABDUL RAFFAY <raffay.ahmed009@gmail.com>
2023-04-16 20:29:15 +0500

    add: motor state APIs done

--- src/controllers/motor-state.controller.js
+++ src/controllers/motor-state.controller.js
@@ -36,8 +36,96 @@ const getOrderingOptionsController = catchAsync(async (req, res) => {
   res.json(response.data);
 });
 
+const getShippingEstimateController = catchAsync(async (req, res) => {
+  const response = await motorStateAPIInstance.request({
+    url: `ShippingEstimate`,
+    method: 'POST',
+    data: req.body,
+    headers: {
+      apiKey: process.env.MOTOR_STATE_API_KEY,
+    },
+  });
+  res.json(response.data);
+});
+
+const getOrderInquiryController = catchAsync(async (req, res) => {
+  const response = await motorStateAPIInstance.request({
+    url: `OrderInquiry`,
+    method: 'POST',
+    data: req.body,
+    headers: {
+      apiKey: process.env.MOTOR_STATE_API_KEY,
+    },
+  });
+  res.json(response.data);
+});
+
+const getOrdersController = catchAsync(async (req, res) => {
+  const { orderNumbers } = req.query;
+  const response = await motorStateAPIInstance.request({
+    url: `Order?orderNumbers=${orderNumbers}`,
+    method: 'GET',
+    headers: {
+      apiKey: process.env.MOTOR_STATE_API_KEY,
+    },
+  });
+  res.json(response.data);
+});
+
+const makeOrderController = catchAsync(async (req, res) => {
+  const response = await motorStateAPIInstance.request({
+    url: `Order`,
+    method: 'POST',
+    data: req.body,
+    headers: {
+      apiKey: process.env.MOTOR_STATE_API_KEY,
+    },
+  });
+  res.json(response.data);
+});
+
+const getBrandsController = catchAsync(async (req, res) => {
+  const response = await motorStateAPIInstance.request({
+    url: `Brands`,
+    method: 'GET',
+    headers: {
+      apiKey: process.env.MOTOR_STATE_API_KEY,
+    },
+  });
+  res.json(response.data);
+});
+
+const getAccountSummaryController = catchAsync(async (req, res) => {
+  const response = await motorStateAPIInstance.request({
+    url: `AccountSummary`,
+    method: 'GET',
+    headers: {
+      apiKey: process.env.MOTOR_STATE_API_KEY,
+    },
+  });
+  res.json(response.data);
+});
+
+const getRetailMarketPlaceStatusController = catchAsync(async (req, res) => {
+  const response = await motorStateAPIInstance.request({
+    url: `RetailMarketPlaceEnabled`,
+    method: 'GET',
+    headers: {
+      apiKey: process.env.MOTOR_STATE_API_KEY,
+    },
+  });
+  res.json(response.data);
+});
+
 module.exports = {
   pingController,
   getProductsController,
   getOrderingOptionsController,
+  getShippingEstimateController,
+  getOrderInquiryController,
+  makeOrderController,
+  getOrdersController,
+  getBrandsController,
+  getAccountSummaryController,
+  getRetailMarketPlaceStatusController,
 };
--- src/routes/v1/motor-state.route.js
+++ src/routes/v1/motor-state.route.js
@@ -9,5 +9,16 @@ const router = express.Router();
 router.route('/ping').get(auth('forUser'), motorStateController.pingController);
 router.route('/products').get(auth('forUser'), motorStateController.getProductsController);
 router.route('/ordering-options').get(auth('forUser'), motorStateController.getOrderingOptionsController);
+router.route('/shipping-estimate').post(auth('forUser'), motorStateController.getShippingEstimateController);
+router.route('/order-inquiry').post(auth('forUser'), motorStateController.getOrderInquiryController);
+router
+  .route('/order')
+  .get(auth('forUser'), motorStateController.getOrdersController)
+  .post(auth('forUser'), motorStateController.makeOrderController);
+router.route('/brands').get(auth('forUser'), motorStateController.getBrandsController);
+router.route('/account-summary').get(auth('forUser'), motorStateController.getAccountSummaryController);
+router
+  .route('/check-retail-marketplace-status')
+  .get(auth('forUser'), motorStateController.getRetailMarketPlaceStatusController);
 
 module.exports = router;
